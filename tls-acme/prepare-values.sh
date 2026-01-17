29ff11f42ed3a2e011bbdee5487e46f33f421739
Pengfei Ding <9438483+dingp@users.noreply.github.com>
2026-01-17 15:35:37 -0800

    refactor tls-acme to use a prepare-values.sh

---
+++ tls-acme/prepare-values.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+values_file="${1:-"${base_dir}/values_template.yaml"}"
+output_file="${2:-"${base_dir}/values.yaml"}"
+
+# Update these values before running this script.
+# Output goes to values.yaml unless you pass a second argument.
+nersc_user_id="<your nersc pid>"
+nersc_user_group="<the gid of the nersc project that you want to run this in>"
+service_port="8080"
+ingress_name="<ingress_name>" # <ingress_name>.<namespace>.<cluster>.svc.spin.nersc.org
+domain="<domain>"
+domain_list="<domain(s)>"
+email="<email>"
+cluster="<cluster>"
+use_case="case2"
+# case 1: use an existing web-server, specify the same port number used by the webserver's cluster IP.
+# case 2: no existing web-server, create one;
+
+webserver_existing="false"
+if [ "${use_case}" = "case1" ]; then
+  webserver_existing="true"
+fi
+
+tmp_file="$(mktemp)"
+sed \
+  -e "s|{{nersc_user_id}}|${nersc_user_id}|g" \
+  -e "s|{{nersc_user_group}}|${nersc_user_group}|g" \
+  -e "s|{{service_port}}|${service_port}|g" \
+  -e "s|{{ingress_name}}|${ingress_name}|g" \
+  -e "s|{{domain}}|${domain}|g" \
+  -e "s|{{domain_list}}|${domain_list}|g" \
+  -e "s|{{email}}|${email}|g" \
+  -e "s|{{cluster}}|${cluster}|g" \
+  -e "s|{{use_case}}|${use_case}|g" \
+  -e "s|{{webserver_existing}}|${webserver_existing}|g" \
+  "$values_file" > "$tmp_file"
+mv "$tmp_file" "$output_file"
